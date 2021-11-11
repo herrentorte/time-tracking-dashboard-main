625aa8ffcac1af40fb5cc00fa97b9606a01a5cb0
herrentorte <84862932+herrentorte@users.noreply.github.com>
2021-11-11 16:08:13 +0100

    Added js, mobile finished, desktop a mess

--- main.js
+++ main.js
@@ -1,1 +1,44 @@
+function dayFunc() {
+  document.getElementById("work-time-current").innerHTML = "5hrs";
+  document.getElementById("work-time-prev").innerHTML = "Yesterday - 7hrs";
+  document.getElementById("play-time-current").innerHTML = "1hrs";
+  document.getElementById("play-time-prev").innerHTML = "Yesterday - 2hrs";
+  document.getElementById("study-time-current").innerHTML = "0hrs";
+  document.getElementById("study-time-prev").innerHTML = "Yesterday - 1hr";
+  document.getElementById("exercise-time-current").innerHTML = "1hr";
+  document.getElementById("exercise-time-prev").innerHTML = "Yesterday - 1hr";
+  document.getElementById("social-time-current").innerHTML = "1hr";
+  document.getElementById("social-time-prev").innerHTML = "Yesterday - 3hrs";
+  document.getElementById("self-time-current").innerHTML = "0hrs";
+  document.getElementById("self-time-prev").innerHTML = "Yesterday - 1hr";
+}
 
+function weekFunc() {
+  document.getElementById("work-time-current").innerHTML = "32hrs";
+  document.getElementById("work-time-prev").innerHTML = "Last Week - 36hrs";
+  document.getElementById("play-time-current").innerHTML = "10hrs";
+  document.getElementById("play-time-prev").innerHTML = "Last Week - 8hrs";
+  document.getElementById("study-time-current").innerHTML = "4hrs";
+  document.getElementById("study-time-prev").innerHTML = "Last Week - 7hrs";
+  document.getElementById("exercise-time-current").innerHTML = "4hrs";
+  document.getElementById("exercise-time-prev").innerHTML = "Last Week - 5hrs";
+  document.getElementById("social-time-current").innerHTML = "5hrs";
+  document.getElementById("social-time-prev").innerHTML = "Last Week - 10hrs";
+  document.getElementById("self-time-current").innerHTML = "2hrs";
+  document.getElementById("self-time-prev").innerHTML = "Last Week - 2hrs";
+}
+
+function monthFunc() {
+  document.getElementById("work-time-current").innerHTML = "103hrs";
+  document.getElementById("work-time-prev").innerHTML = "Last Month - 128hrs";
+  document.getElementById("play-time-current").innerHTML = "23hrs";
+  document.getElementById("play-time-prev").innerHTML = "Last Month - 29hrs";
+  document.getElementById("study-time-current").innerHTML = "13hrs";
+  document.getElementById("study-time-prev").innerHTML = "Last Month - 19hrs";
+  document.getElementById("exercise-time-current").innerHTML = "11hrs";
+  document.getElementById("exercise-time-prev").innerHTML = "Last Month - 18hrs";
+  document.getElementById("social-time-current").innerHTML = "21hrs";
+  document.getElementById("social-time-prev").innerHTML = "Last Month - 23hrs";
+  document.getElementById("self-time-current").innerHTML = "7hrs";
+  document.getElementById("self-time-prev").innerHTML = "Last Month - 11hrs";
+}
